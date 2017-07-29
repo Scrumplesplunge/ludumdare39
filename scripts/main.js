8cfff709eb1631cf102dd7aa7e7500c8303352c0
Joe Fowler <scrumplesplunge@gmail.com>
2017-07-29 16:57:55 +0100

    Platform and one-way barrier.

--- scripts/main.js
+++ scripts/main.js
@@ -81,7 +81,22 @@ class Wizard extends PhysicsObject {
 var universe = new Universe;
 var wizard = new Wizard(new Vector(100, 100));
 universe.add(wizard);
-var boundaries = [
+
+function addShape(boundaries) {
+  if (boundaries.length < 2) return;
+  for (var i = 1, n = boundaries.length; i < n; i++) {
+    var a = boundaries[i - 1];
+    var b = boundaries[i];
+    universe.add(new Boundary(new Vector(a[0], a[1]), new Vector(b[0], b[1])));
+  }
+  var last = boundaries[boundaries.length - 1];
+  var first = boundaries[0];
+  universe.add(new Boundary(new Vector(last[0], last[1]),
+                            new Vector(first[0], first[1])));
+}
+
+// Add the outer boundaries.
+addShape([
   [10, 10],
   [500, 10],
   [500, 300],
@@ -90,13 +105,15 @@ var boundaries = [
   [200, 275],
   [100, 300],
   [10, 300],
-  [10, 10]
-];
-for (var i = 1, n = boundaries.length; i < n; i++) {
-  var a = boundaries[i - 1];
-  var b = boundaries[i];
-  universe.add(new Boundary(new Vector(a[0], a[1]), new Vector(b[0], b[1])));
-}
+]);
+// Add a floating obstacle.
+addShape([
+  [300, 225],
+  [400, 200],
+  [200, 200],
+]);
+// Add an isolated boundary that can only be walked through in one direction.
+universe.add(new Boundary(new Vector(400, 200), new Vector(400, 275)));
 
 var time = 0;
 demoState.on("update", function(event) {
